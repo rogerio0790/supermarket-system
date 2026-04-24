463510ba21e41aa91c0b7312230fd1ce42084475
Roger <rogermuhire8@gmail.com>
2026-04-24 17:16:41 +0300

    fix: update ALLOWED_HOSTS to allow all hosts

--- supermarket_backend/config/settings.py
+++ supermarket_backend/config/settings.py
@@ -6,12 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
-
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1',
-    cast=Csv()
-)
+ALLOWED_HOSTS = ['*']
 
 # ========================
 # Applications

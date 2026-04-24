458cfbcc84fc3c69bc5e7f870d97dda6bc562538
Roger <rogermuhire8@gmail.com>
2026-04-24 17:35:20 +0300

    fix: update ALLOWED_HOSTS to allow all hosts

--- supermarket_backend/config/settings.py
+++ supermarket_backend/config/settings.py
@@ -6,11 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "supermarket-system-3.onrender.com"
-]
+ALLOWED_HOSTS = ["*"]
 
 # ========================
 # Applications
